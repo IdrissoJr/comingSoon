54beed22c4a43be6b34ccfdcdf6bf3202877da3e
Disso <jrD1sso@gmail.com>
2017-12-10 20:45:28 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*global $, jQuery, alert*/
+$(function () {
+    'use strict';
+    $('#countdown').countdown('2018/1/1', function(event) {
+	  var $this = $(this).html(event.strftime(''
+	    + '<span>%w</span> Weeks '
+	    + '<span>%d</span> Days '
+	    + '<span>%H</span> Hr '
+	    + '<span>%M</span> Min '
+	    + '<span>%S</span> Sec'));
+	});
+});
